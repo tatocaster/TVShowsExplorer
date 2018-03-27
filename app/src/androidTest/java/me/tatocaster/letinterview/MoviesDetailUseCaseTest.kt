3dda7f3dcf9e55eb86fa633e84394505baffb38a
Merab Kutalia <kutaliatato@gmail.com>
2018-03-27 22:37:43 +0400

    add instrumented tests for usecases

---
+++ app/src/androidTest/java/me/tatocaster/letinterview/MoviesDetailUseCaseTest.kt
@@ -0,0 +1,36 @@
+package me.tatocaster.letinterview
+
+import me.tatocaster.letinterview.data.DataModule
+import me.tatocaster.letinterview.features.moviesdetail.usecase.MoviesDetailUseCase
+import me.tatocaster.letinterview.features.moviesdetail.usecase.MoviesDetailUseCaseImpl
+import org.junit.Before
+import org.junit.Test
+
+class MoviesDetailUseCaseTest {
+    lateinit var useCase: MoviesDetailUseCase
+
+    @Before
+    fun setUp() {
+        val dataModule = DataModule()
+        useCase = MoviesDetailUseCaseImpl(dataModule.provideApi(dataModule.provideRetrofit()))
+    }
+
+    @Test
+    fun testGetSimilarShows() {
+        useCase.getSimilarShows(1, 1)
+                .test()
+                .await()
+                .assertNoErrors()
+                .assertComplete()
+    }
+
+    @Test
+    fun testGetTvData() {
+        useCase.getTvData(1)
+                .test()
+                .await()
+                .assertNoErrors()
+                .assertComplete()
+                .assertValue { it.overview.isNotBlank() }
+    }
+}
